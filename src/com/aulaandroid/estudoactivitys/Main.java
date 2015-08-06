70edaf5637db32a43a5ed85a3ebf2b2c9c99fe37
dandara-s <dandarasalaun@hotmail.com>
2015-08-06 15:33:37 -0300

    Adicionado Menu

--- src/com/aulaandroid/estudoactivitys/Main.java
+++ src/com/aulaandroid/estudoactivitys/Main.java
@@ -1,33 +1,59 @@
 package com.aulaandroid.estudoactivitys;
 
 import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Main extends ActionBarActivity {
 
+	Button btn;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
-	}
+		
+		btn = (Button) findViewById(R.id.btn);
+		btn.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+			// chamando uma activity
+				Intent i = new Intent(v.getContext(), Tela_2.class);
+				startActivity( i );
+			}
+		});
 
+		  
+	}
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
+	public boolean onCreateOptionsMenu(Menu menu){
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
+		
 	}
-
+	
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
-		int id = item.getItemId();
-		if (id == R.id.action_settings) {
-			return true;
+		switch(item.getItemId()){
+			case R.id.mnTela1:
+				Toast.makeText(this, "Tela 1", Toast.LENGTH_LONG).show();;
+				break;
+				
+			case R.id.mnTela2:
+				Toast.makeText(this, "Tela 2", Toast.LENGTH_LONG).show();;
+				
+				break;
+			case R.id.mnTela3:
+				Toast.makeText(this, "Tela 3", Toast.LENGTH_LONG).show();;
+				
+				break;
 		}
 		return super.onOptionsItemSelected(item);
 	}
--- src/com/aulaandroid/estudoactivitys/Tela_2.java
+++ src/com/aulaandroid/estudoactivitys/Tela_2.java
@@ -1,5 +1,18 @@
 package com.aulaandroid.estudoactivitys;
 
-public class Tela_2 {
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
+public class Tela_2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.tela_2);
+		
+	}
 }
